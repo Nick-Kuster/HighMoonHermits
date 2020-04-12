ae3b2fd9c2cd3b3a1b71f010fa83bb247b9025b4
nick <nsk6@zips.uakron.edu>
2020-04-11 20:48:03 -0400

    Linked To Store

--- HighMoonHermits/src/components/Store.js
+++ HighMoonHermits/src/components/Store.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import PageTemplate from './PageTemplate'
-
+import DizzyJam from './DizzyJam'
 const banner = 'https://i.imgur.com/AUKrLMu.png'
 export default class About extends React.Component {
+
+
     render() {
         return(
-            <PageTemplate banner={ banner }></PageTemplate>
+            <PageTemplate banner={ banner }>                  
+               <iframe 
+                width="100%"
+                height="1200px"
+                src="https://www.dizzyjam.com/embed/highmoonhermits/"/>
+            </PageTemplate>
         )
     }
 }
